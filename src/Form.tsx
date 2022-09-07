5cc399d9abb74b2db59ea42ca3fecb62b60ac759
Donghee Kim <dongheekim@woowahan.com>
2022-09-07 23:02:08 +0900

    feat: countdown 버튼을 클릭하면 1초씩 감소(카운트다운)한다

--- src/Form.tsx
+++ src/Form.tsx
@@ -5,41 +5,77 @@ export default function Form(): JSX.Element {
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
 
+  const [totalSeconds, setTotalSeconds] = useState(0);
+
   return (
     <form
       className="App"
       onSubmit={(e) => {
         e.preventDefault();
+
+        // TODO: validate hours, minutes, seconds
+
+        setTotalSeconds((hours * 60 + minutes) * 60 + seconds);
+
+        const timeId = setInterval(() => {
+          setTotalSeconds((prev) => {
+            if (prev === 0) {
+              clearInterval(timeId);
+              return 0;
+            }
+
+            return prev - 1;
+          });
+        }, 1_000);
       }}
     >
       <input
         type="number"
-        ref={(ref) => ref && (ref.value = "")}
+        ref={(ref) => ref && ref.value === "0" && (ref.value = "")}
         value={removeStartingZero(hours)}
         onChange={(e) => setHours(e.target.valueAsNumber || 0)}
         placeholder="HH"
+        min="0"
+        max="24"
       />
       <span>:</span>
       <input
         type="number"
-        ref={(ref) => ref && (ref.value = "")}
+        ref={(ref) => ref && ref.value === "0" && (ref.value = "")}
         value={removeStartingZero(minutes)}
         onChange={(e) => setMinutes(e.target.valueAsNumber || 0)}
         placeholder="MM"
+        min="0"
+        max="60"
       />
       <span>:</span>
       <input
         type="number"
-        ref={(ref) => ref && (ref.value = "")}
+        ref={(ref) => ref && ref.value === "0" && (ref.value = "")}
         value={removeStartingZero(seconds)}
         onChange={(e) => setSeconds(e.target.valueAsNumber || 0)}
         placeholder="SS"
+        min="0"
+        max="60"
       />
       <button type="submit">Countdown</button>
+      <p>{toTimeString(totalSeconds)}</p>
     </form>
   );
 }
 
 function removeStartingZero(num: number): string {
   return num.toString().replace(/^(0+)[^0]/, "");
+}
+
+function toTimeString(time: number): string {
+  const seconds = time % 60;
+  time = Math.floor(time / 60);
+  const minutes = time % 60;
+  const hours = Math.floor(time / 60);
+
+  return [hours, minutes, seconds]
+    .map((num) => num.toString())
+    .map((str) => str.padStart(2, "0"))
+    .join(":");
 }
