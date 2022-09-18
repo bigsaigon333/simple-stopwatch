a4a62a1eb504814a967028b9841a6478c894e7c1
Donghee Kim <dongheekim@woowahan.com>
2022-09-18 12:40:56 +0900

    feat: non-edit 모드에서 앞단의 0 를 hidden처리한다

--- src/TimeString.tsx
+++ src/TimeString.tsx
@@ -1,4 +1,5 @@
 import { css } from "@emotion/react";
+import TimeCharacter, { TimeCharacterProps } from "./TimeCharacter";
 
 export default function TimeString({
   value,
@@ -13,17 +14,48 @@ export default function TimeString({
 }): JSX.Element {
   const pad = "0".repeat(6 - value.length);
 
-  const [H1, H2, M1, M2, S1, S2] = [...pad, ...value].map((char, index) => (
-    <span
-      css={css`
-        font-size: 3rem;
-        ${index < pad.length ? "color: gray;" : "color: white;"}
-        ${index === 5 && focused && "border-right: 1px solid white;"}
-      `}
-    >
-      {char}
-    </span>
-  ));
+  const zeroPad = !dirty ? /^0*/.exec(value)?.[0] ?? "" : "";
+
+  const [H1, H2, M1, M2, S1, S2] = [...pad, ...value].map<TimeCharacterProps>(
+    (char, index) => ({
+      color: index < pad.length ? "gray" : "white",
+      visible:
+        dirty ||
+        index >= zeroPad.length ||
+        (index == 5 && zeroPad.length === 6),
+      cursor: index === 5 && focused,
+      char,
+      index,
+      fontSize: "large",
+    })
+  );
+
+  const hourMeasure: TimeCharacterProps = {
+    color: value.length > 4 ? "white" : "gray",
+    visible: dirty || zeroPad.length < 2,
+    marginLeft: "1px",
+    marginRight: "2px",
+    char: "h",
+    fontSize: "medium",
+  };
+
+  const minuteMeasure: TimeCharacterProps = {
+    color: value.length > 2 ? "white" : "gray",
+    visible: dirty || zeroPad.length < 4,
+    marginLeft: "1px",
+    marginRight: "2px",
+    char: "m",
+    fontSize: "medium",
+  };
+
+  const secondMeasure: TimeCharacterProps = {
+    color: value.length > 0 ? "white" : "gray",
+    visible: dirty || zeroPad.length <= 6,
+    marginLeft: focused ? "1px" : 0,
+    marginRight: "2px",
+    char: "s",
+    fontSize: "medium",
+  };
 
   return (
     <div
@@ -53,39 +85,11 @@ export default function TimeString({
       `}
       onClick={onClick}
     >
-      {H1}
-      {H2}
-      <span
-        css={css`
-          margin-left: 1px;
-          margin-right: 2px;
-          ${value.length > 4 ? "color: white;" : "color: gray;"};
-        `}
-      >
-        h
-      </span>
-      {M1}
-      {M2}
-      <span
-        css={css`
-          margin-left: 1px;
-          margin-right: 2px;
-          ${value.length > 2 ? "color: white;" : "color: gray;"}
-        `}
-      >
-        m
-      </span>
-      {S1}
-      {S2}
-      <span
-        css={css`
-          ${!focused && "margin-left: 1px;"}
-          margin-right: 2px;
-          ${value.length > 0 ? "color: white;" : "color: gray;"}
-        `}
-      >
-        s
-      </span>
+      {[H1, H2, hourMeasure, M1, M2, minuteMeasure, S1, S2, secondMeasure].map(
+        (props, index) => (
+          <TimeCharacter key={props.char + index} {...props} />
+        )
+      )}
     </div>
   );
 }
