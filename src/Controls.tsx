7d995eced7200f7627f40cd2297cea83c957821e
Donghee Kim <dongheekim@woowahan.com>
2022-09-18 13:59:44 +0900

    refactor: 버튼들을 Controls 컴포넌트로 분리한다

---
+++ src/Controls.tsx
@@ -0,0 +1,71 @@
+import { css } from "@emotion/react";
+import Audio from "./Audio";
+import useBoolean from "./hooks/useBoolean";
+import {
+  TimerState,
+  useTimerState,
+  useTimerStateDispatch,
+} from "./timerContext";
+import { toTotalSeconds } from "./utils";
+
+export default function Controls({ value }: { value: string }): JSX.Element {
+  const dispatch = useTimerStateDispatch();
+  const timerState = useTimerState();
+  const [muted, , , toggle] = useBoolean(false);
+
+  return (
+    <div
+      css={css`
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+      `}
+    >
+      <button css={buttonCss} type="button" onClick={() => toggle()}>
+        {muted ? "unmute" : "mute"}
+      </button>
+      <button
+        css={css`
+          ${buttonCss}
+          color: red;
+        `}
+        type="button"
+        onClick={() => dispatch("edit")}
+      >
+        Reset
+      </button>
+      <button
+        css={buttonCss}
+        type="submit"
+        disabled={timerState !== "done" && toTotalSeconds(value) <= 0}
+      >
+        {getMainButtonMessage(timerState)}
+      </button>
+      <Audio play={timerState === "done"} muted={muted} />
+    </div>
+  );
+}
+
+function getMainButtonMessage(state: TimerState): string {
+  switch (state) {
+    case "edit":
+      return "Start";
+    case "ticking":
+      return "Pause";
+    case "paused":
+      return "Resume";
+    case "done":
+      return "Ok";
+    default:
+      throw new Error(`Unexpected state: ${state}`);
+  }
+}
+
+const buttonCss = css`
+  width: 8rem;
+  height: 3.5rem;
+  border-radius: 1rem;
+  font-size: 1.5rem;
+  font-weight: 600;
+  padding: 0;
+`;
