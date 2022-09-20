21440ae452b3df42abab79b6d1c05b82d9578fdf
Donghee Kim <dongheekim@woowahan.com>
2022-09-20 23:45:33 +0900

    fix: react-helmet-async의 HelmetProvider를 제거한다

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
 import App from "./app";
 import { TimerProvider } from "./contexts/timer-context";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.querySelector("#root") as HTMLElement).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <TimerProvider>
-        <App />
-      </TimerProvider>
-    </HelmetProvider>
+    <TimerProvider>
+      <App />
+    </TimerProvider>
   </React.StrictMode>
 );
