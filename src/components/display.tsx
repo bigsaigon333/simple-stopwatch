3418dadf540b44c86f8671680315e816f8f5cd66
Donghee Kim <dongheekim@toss.im>
2022-11-27 17:52:11 +0900

    feat: Display 컴포넌트 구현

--- src/components/display.tsx
+++ src/components/display.tsx
@@ -1,34 +1,47 @@
+import { css } from "@emotion/react";
 import { toTimeTuple } from "../utils/utils";
 
 interface Properties {
   time: number;
 }
 
+const HOUR_MEASURE = "h";
+const MINUTE_MEASURE = "m";
+const SECOND_MEASURE = "s";
+
 const Display = ({ time }: Properties) => {
   const [hours, minutes, seconds] = toTimeTuple(time);
 
   return (
     <div>
-      {hours && (
-        <>
-          <span>{hours}</span>
-          <span>h</span>
-        </>
-      )}
-      {minutes && (
-        <>
-          <span>{minutes}</span>
-          <span>m</span>
-        </>
-      )}
-      {seconds && (
-        <>
-          <span>{seconds}</span>
-          <span>s</span>
-        </>
-      )}
+      {[
+        ...hours,
+        HOUR_MEASURE,
+        ...minutes,
+        MINUTE_MEASURE,
+        ...seconds,
+        SECOND_MEASURE,
+      ].map((char, index) => (
+        <Display.Char key={char + index} char={char} />
+      ))}
     </div>
   );
 };
 
 export default Display;
+
+// eslint-disable-next-line react/display-name
+Display.Char = ({ char }: { char: string }): JSX.Element => {
+  const isNumber = /^\d$/.test(char);
+
+  return (
+    <span
+      css={css`
+        font-size: ${isNumber ? "3rem" : "2rem"};
+        font-weight: 500;
+      `}
+    >
+      {char}
+    </span>
+  );
+};
