e6f0da94d5d7846adb826a1bd833433ce13deee8
Donghee Kim <dongheekim@toss.im>
2022-11-27 18:01:27 +0900

    refactor: named export 사용

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import App from "./app";
 import { TimerProvider } from "./contexts/timer-context";
 import "./styles/index.css";
 
-ReactDOM.createRoot(document.querySelector("#root") as HTMLElement).render(
-  <React.StrictMode>
+createRoot(document.querySelector("#root") as HTMLElement).render(
+  <StrictMode>
     <TimerProvider>
       <App />
     </TimerProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
